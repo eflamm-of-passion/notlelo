cdd376989883988564c815a524e5f6a272abc384
koldeskient <eflamm.ollivier@gmail.com>
2022-01-23 20:52:06 +0100

    display header bar

--- app/src/main/java/io/eflamm/notlelo/views/HeaderView.kt
+++ app/src/main/java/io/eflamm/notlelo/views/HeaderView.kt
@@ -1,9 +1,37 @@
 package io.eflamm.notlelo.views
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import io.eflamm.notlelo.CameraActivity
+import io.eflamm.notlelo.HomeActivity
+import io.eflamm.notlelo.R
 
-class HeaderView(context: Context, attributes: AttributeSet): View(context, attributes) {
+class HeaderView: ConstraintLayout {
 
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context,
+                attrs: AttributeSet,
+                defStyle: Int) : super(context, attrs, defStyle) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.view_header, this)
+    }
+
+    fun onClickBackButton (view : View) {
+        val activity = context as Activity
+        activity.finish()
+    }
 }
